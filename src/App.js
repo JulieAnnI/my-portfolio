a0b5385c40a39ff6ce39b0c6d90501835dc4f992
Julie Ann <julieanniwinski@gmail.com>
2021-03-30 20:47:50 -0400

    Added routes for pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavTabs from "./pages/NavTabs";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Router>
+      <div>
+        <NavTabs />
+        <Route exact path="/" component={About} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path = "/contact" component={Contact} />  
+      </div>
+    </Router>
   );
 }
 
