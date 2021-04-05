f1fb07e45e28994515b8b08514d77706d02fc982
Julie Ann <julieanniwinski@gmail.com>
2021-04-05 13:25:26 -0400

    Added footer and footer styling

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+
+    return (
+        <footer className="footer">
+            <div>@2021 Julie Ann Iwinski</div>
+            <div>julieanniwinski@gmail.com</div>
+            <a href="https://github.com/JulieAnnI" title="Github" className="footer-link"
+            >Github</a>
+        </footer>
+    );
+}
+
+export default Footer;
