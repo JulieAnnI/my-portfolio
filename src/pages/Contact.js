fb50885d201774649fc4bd996602c73b80e6f76c
Julie Ann <julieanniwinski@gmail.com>
2021-04-06 22:09:25 -0400

    Added all components and code

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,23 @@
 import React from "react";
+import Container from "../components/Container";
+import Button from "../components/Button";
 
 function Contact() {
     return (
-        <div></div>
+        <Container>
+            <h1>Contact Me</h1>
+                <div className="mb-3">
+                    <label for="exampleFormControlInput1" className="form-label">Email address</label>
+                    <input type="email" className="form-control" id="exampleFormControlInput1"/>
+                </div>
+                <div className="mb-3">
+                    <label for="exampleFormControlTextarea1" className="form-label">Message</label>
+                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+                <Button 
+                    landing="Submit"
+                />
+        </Container>
     );
 }
 
