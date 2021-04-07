9cd60312e7ac1a7f25e1deca0f4d48aea4f8f95e
Julie Ann <julieanniwinski@gmail.com>
2021-04-06 22:05:25 -0400

    Updated file paths

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import NavTabs from "./components/Navbar/NavTabs";
+import NavTabs from "./components/Navbar";
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
-import Footer from "./components/Footer/Footer";
+import Footer from "./components/Footer";
 
 function App() {
   return (
