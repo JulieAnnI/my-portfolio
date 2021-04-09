a009c812c9288dcacf24bc9705032c4ad96c7602
Julie Ann <julieanniwinski@gmail.com>
2021-04-08 21:19:31 -0400

    Fixed bug finally!!!

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ function App() {
         <NavTabs />
         <Route exact path="/" component={About} />
         <Route exact path="/my-portfolio" component={About} />
-        <Route exact path="/my-portfolio/" component={About} />
         <Route exact path="/portfolio" component={Portfolio} />
         <Route exact path = "/contact" component={Contact} /> 
         <Footer /> 
